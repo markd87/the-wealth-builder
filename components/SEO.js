db0418a92c1c9e36acf4a205648cb70c5795386e
Mark Danovich <mark.danovich@gmail.com>
2023-02-22 23:55:35 +0000

    added monetag

--- components/SEO.js
+++ components/SEO.js
@@ -8,6 +8,7 @@ export default function SEO({ title, description, image }) {
       <meta property="og:title" content={title} />
       <meta property="og:image" content={image} />
       <meta name="robots" content="index, follow" />
+      <meta name="monetag" content="70ccbe930069abd378084c17b9e6621e" />
     </Head>
   );
 }
