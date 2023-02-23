5bd9accdc610ffe8a6ef7f0c722ee46e96877afb
Mark Danovich <mark.danovich@gmail.com>
2023-02-23 11:56:48 +0000

    added meta tags

--- components/SEO.js
+++ components/SEO.js
@@ -6,7 +6,13 @@ export default function SEO({ title, description, image }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="artice" />
       <meta property="og:image" content={image} />
+      <meta property="og:site_name" content="The Richest Mind Blog" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
       <meta name="robots" content="index, follow" />
       <meta name="monetag" content="70ccbe930069abd378084c17b9e6621e" />
       <link rel="icon" type="image/jpg" href="images/favicon.ico" />
