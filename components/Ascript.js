f5bbe7ed11f3e7be0b91186dbe7aebba794ac6bc
Mark Danovich <mark.danovich@gmail.com>
2023-03-03 18:55:46 +0000

    added tiquet banner

--- components/Ascript.js
+++ components/Ascript.js
@@ -3,7 +3,8 @@ import Script from 'next/script';
 export default function Ascript({ src }) {
   return (
     <>
-      <Script src={src} charset="utf-8" />
+      <Script src={src} charset="utf-8" strategy="lazyOnload" />
+      <div id="search-widget"></div>
     </>
   );
 }
