5a855262541ae0fd8df5c9ecc6a24ec49b9157c6
Mark Danovich <mark.danovich@gmail.com>
2023-03-24 13:11:01 -0500

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -79,9 +79,9 @@ export default function PostPage({
           <br />
           <div className="sharethis-inline-share-buttons"></div>
           <br/>
-          <!-- Ezoic - bottom_of_page - bottom_of_page -->
-<div id="ezoic-pub-ad-placeholder-104"> </div>
-<!-- End Ezoic - bottom_of_page - bottom_of_page -->
+//           <!-- Ezoic - bottom_of_page - bottom_of_page -->
+          <div id="ezoic-pub-ad-placeholder-104"> </div>
+//           <!-- End Ezoic - bottom_of_page - bottom_of_page -->
           <amp-ad
             width="100vw"
             height="320"
